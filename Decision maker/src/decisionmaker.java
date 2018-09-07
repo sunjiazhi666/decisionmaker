a627bf63fd512c3795ae40720199ed0933b3bb6e
jiazhi.sun <jiazhi.sun@mu-c1-16.mullen.pvt.k12.co.us>
2018-09-07 14:58:11 -0600

    B

--- Decision maker/src/decisionmaker.java
+++ Decision maker/src/decisionmaker.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 
 public class decisionmaker
 	{
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				
+				System.out.println("How many choice do you have?");
+				Scanner userInput1 = new Scanner(System.in);
+				int howmany = userInput1.nextInt();
+				
+				String[] choice = new String [howmany];
+				
 
 			}
 
